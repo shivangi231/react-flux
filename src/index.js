0674f7b73b44f15d1abe01454fb94a97bbc92508
Shivangi <Shivangi@slsplap213-2.local>
2018-12-26 21:59:18 +0530

    Structure changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
-import App from './js/components/app';
+import App from './component-flux/components/app';
 
 ReactDOM.render(
   <App/>,
